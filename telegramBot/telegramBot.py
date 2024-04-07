12abb0484906b756ac18427ae4dbe7600f7dad46
user <aaanonmhaaa@gmail.com>
2024-04-07 09:58:17 +0400

    edited responses

--- telegramBot/telegramBot.py
+++ telegramBot/telegramBot.py
@@ -1,33 +1,37 @@
+from email.headerregistry import ContentTypeHeader
 from typing import Final
-import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import Update
-from telegram.txt import Application, CommandFandler, MessageHandler, filters, ContextTypes
-
 
 TOKEN: Final = '7004939873:AAG2vPh9WkILSM1KiNzPjVNcdaXJwUtbaA8'
 BOT_USERNAME: Final = 'meebeg_bot'
 
 #Commands
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start_command(update: Update, context: ContentTypeHeader.DEFAULT_TYPE):
     await update.message.reply_text('Hey!Thanks for chatting with me! My name is Banana!')
     
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def help_command(update: Update, context: ContentTypeHeader.DEFAULT_TYPE):
     await update.message.reply_text('I am Banana! How can I help you?') 
     
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def custom_command(update: Update, context: ContentTypeHeader.DEFAULT_TYPE):
     await update.message.reply_text('Send me a picture and wait for the emojis!!!!!!!!!!!!!!') 
     
 #Responses
 def handle_response(text: str) -> str:
-    processed: str = text.lower()
-    
-if 'hello' in processed:
-    return 'Hey there!'
+    processed = text.lower()
 
-if 'whats up' or 'how are you' in processed:
-    return 'M ok, what about u?'
+    if 'hello' in processed:
+        return 'Hey there!'
 
-if 'I am ok too' or 'Good' in processed:
-    return 'Your welcome ;)'
-if 'Thank you' or 'Thanks' in processed:
-    return 'Nice to meet you!'
+    elif 'whats up' in processed or 'how are you' in processed:
+        return 'I am okay, what about you?'
+
+    elif 'i am ok too' in processed or 'good' in processed:
+        return 'You\'re welcome ;)'
+
+    elif 'thank you' in processed or 'thanks' in processed:
+        return 'You\'re welcome'
+
+    else:
+        return 'I am sorry, I didn\'t understand that.'
+
